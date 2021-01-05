e8986798bdc342914d64d4441320009516208a15
repen <9keepa@gmail.com>
2021-01-05 22:41:16 +0500

    name changed

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 import os
 
 BASE_DIR = os.getenv("BASE_DIR", False)
-PRODUCTION_WORK = os.getenv("PRODUCTION_WORK", False)
+PRODUCTION_WORK = os.getenv("EXTERNAL_WORK", False)
 DATA_DIR = os.getenv("DATA", False)
